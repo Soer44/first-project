59d4d55354f75154f9ed36612410d34bf2d63263
Egor <volga_egor@mail.ru>
2022-01-24 20:28:54 +0300

    FirstCommit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer + 5);
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Фамилия', '');
+answers[2] = prompt('Сколько лет', '');
+
+document.write(answers);
